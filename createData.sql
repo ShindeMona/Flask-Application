455a5356777cc6d014f5d641d28c03fc11702e67
Shinde Monali Ajinath <vp.monali@gmail.com>
2021-09-16 13:03:14 +0530

    Add files via upload

---
+++ createData.sql
@@ -0,0 +1,4 @@
+create database Monali;
+use Monali;
+create table search(id int AUTO_INCREMENT PRIMARY KEY,search2 varchar(40) NOT NULL ,creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+insert into search(search2) values('Monali');
